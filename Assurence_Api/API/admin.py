979289fc8da0e39d8e992d5f4275e9249e17d7ed
Desos <desnduwim26@gmail.com>
2021-02-23 14:21:38 +0200

    models registered in admin

--- Assurence_Api/API/admin.py
+++ Assurence_Api/API/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Profile)
+# admin.site.register(Photo)
+admin.site.register(Payment)
+admin.site.register(Automobile)
+admin.site.site_header = "Assurence Automobile"
